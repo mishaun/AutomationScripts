f1c838a7e9d53cdd6f29a5615cd67817ca8f3a9b
Mishaun <mishaun@magnumproducing.com>
2020-02-12 17:48:15 -0600

    added code for pdf writing

--- BLM.py
+++ BLM.py
@@ -120,16 +120,42 @@ wb.close()
 # conda install -c conda-forge pdfrw
 import pdfrw
 
+INVOICE_TEMPLATE_PATH = 'bidsheet template.pdf'
+INVOICE_OUTPUT_PATH = '/Bid Sheets/bidsheet.pdf'
+
+
+ANNOT_KEY = '/Annots'
+ANNOT_FIELD_KEY = '/T'
+ANNOT_VAL_KEY = '/V'
+ANNOT_RECT_KEY = '/Rect'
+SUBTYPE_KEY = '/Subtype'
+WIDGET_SUBTYPE_KEY = '/Widget'
+
+
+def write_fillable_pdf(input_pdf_path, output_pdf_path, data_dict):
+    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    annotations = template_pdf.pages[0][ANNOT_KEY]
+    for annotation in annotations:
+        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+            if annotation[ANNOT_FIELD_KEY]:
+                key = annotation[ANNOT_FIELD_KEY][1:-1]
+                if key in data_dict.keys():
+                    annotation.update(
+                        pdfrw.PdfDict(V='{}'.format(data_dict[key]))
+                    )
+    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+
+
 fields = {
-        "State": "",
-        "Date of Sale": "",
-        'Check Box for Oil and Gas' : "",
+        "State": stinitials,
+        "Date of Sale": date,
+        'Check Box for Oil and Gas' : "x",
         "Oil and Gas/Parcel No" : "",
         "TOTAL BID FOR Oil and Gas Lease" : "",
         "PAYMENT SUBMITTED WITH BID for Oil and Gas" : "",
-        "Print or Type Name of Lessee" : "",
-        "Address of Lessee": "",
-        "City" : "",
-        "State_2": "",
-        "Zip Code" : ""
+        "Print or Type Name of Lessee" : "R&R Royalty, LTD",
+        "Address of Lessee": "500 N Shoreline Blvd, Ste 322",
+        "City" : "Corpus Christi",
+        "State_2": "TX",
+        "Zip Code" : "78401"
         }
